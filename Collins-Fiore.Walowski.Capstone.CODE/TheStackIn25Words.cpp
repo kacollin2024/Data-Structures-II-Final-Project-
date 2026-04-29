abd13df56728e5aa67f4b072963431d2a174b278
Kailey Collins Fiore <kacollin2024@students.curry.edu>
2026-04-29 19:01:32 -0400

    TheStack file
    Started the Stack file

---
+++ Collins-Fiore.Walowski.Capstone.CODE/TheStackIn25Words.cpp
@@ -0,0 +1,51 @@
+#include "TheStackIn25Words.h"
+#include <string>
+#include <iostream>
+#include "Node.h"
+using namespace std; 
+
+TheStackIn25Words::TheStackIn25Words()
+{
+    top = nullptr;
+    count=0;
+}
+
+TheStackIn25Words::~TheStackIn25Words()
+{
+    while (!stackIsEmpty())
+    {
+        pop();
+    }
+}
+
+void TheStackIn25Words::push(string word)
+{
+    Node* newNode = new Node(word);
+
+    if (stackIsEmpty())
+    {
+        top = newNode;
+    }
+    else
+    {
+        newNode->next = top;
+        top = newNode;
+    }
+    count++;
+}
+
+string TheStackIn25Words::pop()
+{
+    if (stackIsEmpty())
+    {
+    return "";
+    }
+    Node* current = top;
+    string word = current->getData();
+    top = top->next;
+    delete current;
+    count--;
+    return word;
+}
+
+
