7d52dda9db876723189a7b85717f1fae608e7633
Kailey Collins Fiore <kacollin2024@students.curry.edu>
2026-04-29 18:42:15 -0400

    Program runner
    Added in the program runner to the code

---
+++ Collins-Fiore.Walowski.Capstone.CODE/Collins-Fiore.Walowski.Capstone.Stubs.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+// Made by Kailey Collins-Fiore and Samuel Walowski
+
+#include "Menu.h"
+
+int main()
+{
+    Menu menu;
+    menu.addWordsToStack();
+    menu.menu();
+    return 0;
+}
